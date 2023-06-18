1ec9bf701757c119d43ec865afe19abf3196d9b4
W-McPhail <william.m.mcphail@gmail.com>
2023-06-18 17:31:03 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Book William McPhail!</h1>
-      <h2>I would like some $!</h2>
+      <h1>William McPhail - Dog Walker EUS!</h1>
+      <h2>Text 914-806-1544 to book!</h2>
+      <p>I am a senior at Hunter College studying Computer Science. </p>
+      <p>I have hosted dogs on Rover for 6 months and have a stellar record.</p>
+      <p>I walk on Wag as well, but am attempting to move away from that platform.</p>
       <Calendar />
     </div>
   )
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -19,104 +19,316 @@ const Calendar = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="time">8 a.m.</td>
-                        <Event event="hire me?" location="NYC" color="green"/>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">9 a.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
                         <td className="time">10 a.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">11 a.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">12 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">1 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">2 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">3 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">4 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                     <tr>
                         <td className="time">5 p.m.</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">6.pm</td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Unavailable" location="Class" color="green"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">7 p.m.</td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">8 p.m.</td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 p.m.</td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 p.m.</td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
+                        <td>
+                            <Event event="Available" location="EUS/ Manhatten" color="blue"/>
+                        </td>
                     </tr>
                 </tbody>
             </table>
