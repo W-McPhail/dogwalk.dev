de8b4e1cfcae8958bdb0762e4beb2cdf7b412be0
W-McPhail <william.m.mcphail@gmail.com>
2023-06-19 18:45:57 -0400

    change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>William McPhail - Dog Walker EUS!</h1>
+      <h1>William McPhail - Dog Walker UES!</h1>
       <h2>Text 914-806-1544 to book!</h2>
       <p>I am a senior at Hunter College studying Computer Science. </p>
       <p>I have hosted dogs on Rover for 6 months and have a stellar record.</p>
